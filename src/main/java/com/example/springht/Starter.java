9d4668fd646578c2f783d9dacdf644573a373579
SergeyAltshuler <altshuler-it@mail.ru>
2021-07-26 18:51:19 +0300

    task 20_07 - final version

--- src/main/java/com/example/springht/Starter.java
+++ src/main/java/com/example/springht/Starter.java
@@ -1,7 +1,21 @@
 package com.example.springht;
 
-public class Starter {
-    public static void main(String[] args) {
+import com.example.springht.model.Course;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+public class Starter {
+    private static final ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("SpringConfig.xml");
+
+    public static void main(String[] args) {
+        displayCourse("courseMore5");
+        displayCourse("courseLess5");
+        displayCourse("courseMax");
+        displayCourse("courseMin");
+    }
+
+    private static void displayCourse(String beanName) {
+        Course course = context.getBean(beanName, Course.class);
+        System.out.println(course);
+        course.printStudents();
     }
 }
